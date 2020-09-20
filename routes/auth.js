688927b6f6a21a2c379028562ed624a326774a0a
Casey <caseyclinga@gmail.com>
2020-09-20 14:42:49 -0700

    Installed and implemented bcrypt for hashing password

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,28 @@
 const router = require('express').Router();
-// const { restart } = require('nodemon');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 const { registerValidation, loginValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
   //Validate data before adding user
   const { error } = registerValidation(req.body);
-  console.log(error);
   if (error) return res.status(400).send(error.details[0].message);
+
+  //Check if user already exists
+  const emailExists = await User.findOne({ email: req.body.email });
+  if (emailExists) return res.status(400).send('Email already exists');
+
+  //Hash passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  console.log(
+    `Plain Text: ${req.body.password}\nHashed Password: ${hashedPassword}`
+  );
 
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
 
   try {
@@ -20,7 +30,6 @@ router.post('/register', async (req, res) => {
     res.send(savedUser);
   } catch (err) {
     res.status(400).send(err);
-    // console.log(err);
   }
 });
 
