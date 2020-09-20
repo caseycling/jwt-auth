54034c19ef473e247b82042242586c8988e43b7f
Casey <caseyclinga@gmail.com>
2020-09-19 17:32:42 -0700

    Added validation with npm @hapi/joi

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,20 @@ const router = require('express').Router();
 // const { restart } = require('nodemon');
 const User = require('../models/User');
 
+//Validation
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
+});
+
 router.post('/register', async (req, res) => {
+  //Validate data before adding user
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
